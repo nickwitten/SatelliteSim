8a105a37dc1061e4fdcd0b047c1e4545afd4dfbb
Nick Witten <nwitt12@gmail.com>
2022-11-30 18:31:15 -0500

    sdr floating point error, rx and tx wrappers, constellation plot

--- sdr.py
+++ sdr.py
@@ -5,16 +5,76 @@ from scipy.signal import butter, lfilter, freqz
 
 
 NUM_SUB_CARRIERS = 8  # Number of sub-carriers in the OFDM channel
-E_SQRT = 5  # Magnitude of constellation vectors
+EB = 5  # Energy per bit
 SCS = 60e3 # Sub-carrier spacing in Hz
-CARRIER_FREQ = 60e6  # Carrier frequency in Hz
+CARRIER_FREQ = 20e9  # Carrier frequency in Hz
 TS = 1 / (2 * SCS * NUM_SUB_CARRIERS)  # DAC/ADC sample rate
-SIMULATION_TS = 1 / (16 * CARRIER_FREQ)
-CARRIER_SAMPLES_I = np.array([np.cos(2 * np.pi * CARRIER_FREQ * t) for t in
-    np.arange(0, NUM_SUB_CARRIERS * TS, SIMULATION_TS)])  # Simulation in-phase "continuous" samples
-CARRIER_SAMPLES_Q = np.array([np.sin(2 * np.pi * CARRIER_FREQ * t) for t in
-    np.arange(0, NUM_SUB_CARRIERS * TS, SIMULATION_TS)])  # Simulation quadrature "continuous" samples
+SIMULATION_TS = 1 / (4 * CARRIER_FREQ)
+NUM_SIMULATION_SAMPLES_BLOCK = NUM_SUB_CARRIERS * int(TS / SIMULATION_TS)  # Number of "continous" samples per block
+NUM_BITS_BLOCK = NUM_SUB_CARRIERS * 2  # Number of bits per block
+CARRIER_SAMPLES_I = np.array([np.cos(2 * np.pi * CARRIER_FREQ * n * SIMULATION_TS) for n in
+    range(NUM_SIMULATION_SAMPLES_BLOCK)], dtype=np.float64)  # Simulation in-phase "continuous" samples
+CARRIER_SAMPLES_Q = np.array([np.sin(2 * np.pi * CARRIER_FREQ * n * SIMULATION_TS) for n in
+    range(NUM_SIMULATION_SAMPLES_BLOCK)], dtype=np.float64)  # Simulation quadrature "continuous" samples
+RX_CONSTELLATION_BUFFER = []  # Used to hold the latest received symbol vectors for plotting
 
+
+def tx(data: bytes):
+    """ Simulate SDR transmission path from binary data to a continuous time
+    signal. Must pass in an integer number of blocks of data bits. Returns an
+    array of "continuous" samples that are ready to be transmitted into a
+    channel. """
+    # Calculate the number of blocks to transmit
+    num_blocks = len(data) * 8 / (NUM_SUB_CARRIERS * 2)
+    # Check that there is an integer number of blocks to transmit
+    assert int(num_blocks) == num_blocks
+    num_blocks = int(num_blocks)
+    # Allocate a "continuous" time array
+    tx_continuous_samples = np.zeros(num_blocks * NUM_SIMULATION_SAMPLES_BLOCK)
+    # Loop through the blocks and apply all SDR operations
+    for i in range(num_blocks):
+        tx_symbols = symbol_map_tx(
+            data[i*int(NUM_BITS_BLOCK / 8): \
+                 (i+1)*int(NUM_BITS_BLOCK / 8)]
+        )
+        tx_baseband_samples = ifft_tx(tx_symbols)
+        tx_continuous_samples[i * NUM_SIMULATION_SAMPLES_BLOCK: \
+                              (i+1) * NUM_SIMULATION_SAMPLES_BLOCK] = \
+            up_converter_tx(tx_baseband_samples)
+    return tx_continuous_samples
+
+def rx(continuous_samples: list):
+    """ Simulate SDR receive path from a continuous time signal to binary data.
+    Must pass in "continuous" time samples of the received signal. Returns an
+    array of binary data. """
+    global RX_CONSTELLATION_BUFFER
+    # Calculate the number of blocks received
+    num_blocks = continuous_samples.size / NUM_SIMULATION_SAMPLES_BLOCK
+    # Check that there is an integer number of blocks received
+    assert int(num_blocks) == num_blocks
+    num_blocks = int(num_blocks)
+    # Allocate binary data array
+    rx_data = []
+    # Reset the constellation buffer
+    RX_CONSTELLATION_BUFFER = []
+    # Loop through the blocks and apply all SDR operations
+    for i in range(num_blocks):
+        rx_baseband_samples = down_converter_rx(
+            continuous_samples[i * NUM_SIMULATION_SAMPLES_BLOCK: \
+                               (i+1) * NUM_SIMULATION_SAMPLES_BLOCK]
+        )
+        rx_vectors = fft_rx(rx_baseband_samples)
+        rx_data.append(symbol_map_rx(rx_vectors))
+        RX_CONSTELLATION_BUFFER.append(rx_vectors)
+    return rx_data
+
+def plot_rx_constellation():
+    """ Plot the latest received signal vectors. Must be called after rx() """
+    assert len(RX_CONSTELLATION_BUFFER) != 0
+    for array in RX_CONSTELLATION_BUFFER:
+        plt.scatter(np.real(array), np.imag(array))
+    plt.grid()
+    plt.show()
 
 def symbol_map_tx(data: bytes):
     """ Map 16 bit stream onto 8 complex vectors using QPSK bit mapping 
@@ -29,7 +89,7 @@ def symbol_map_tx(data: bytes):
             ((data[byte_index] >> (i % 8 + 1)) & 1) * 2 - 1
         )
         symbols[int(i / 2)] = symbol
-    return symbols * E_SQRT
+    return symbols * np.sqrt(EB)
 
 def symbol_map_rx(symbols: list):
     """ Return 16 bit stream from 8 complex vectors using QPSK bit mapping and
@@ -74,9 +134,7 @@ def down_converter_rx(continuous: list):
     # Multiply by carrier frequency
     demodulated_i = continuous * CARRIER_SAMPLES_I
     demodulated_q = continuous * CARRIER_SAMPLES_Q
-    # Convolve with low pass filter to remove double frequency and out of band noise
-    b, a = scipy.signal.iirfilter(4, Wn=(1.5 * NUM_SUB_CARRIERS * SCS),
-            fs=(1 /SIMULATION_TS), btype="low", ftype="butter")
+    b, a = butter(3, 6 * NUM_SUB_CARRIERS * SCS, fs=(1/SIMULATION_TS), btype='low', analog=False)
     demodulated_i = lfilter(b, a, demodulated_i)
     demodulated_q = lfilter(b, a, demodulated_q)
     # Sample the signals to simulate ADC conversion
@@ -89,18 +147,18 @@ def down_converter_rx(continuous: list):
 
 
 if __name__ == '__main__':
-    data = "Hi".encode()
+    data = "Hello World!".encode()
     print(f"Sending {data.decode()}")
 
-    tx_symbols = symbol_map_tx(data)
-    tx_baseband_samples = ifft_tx(tx_symbols)
-    tx_continuous_samples = up_converter_tx(tx_baseband_samples)
+    continuous_samples = tx(data)
+    rx_data = rx(continuous_samples)
 
-    plt.plot(np.arange(0, tx_continuous_samples.size * SIMULATION_TS, SIMULATION_TS), tx_continuous_samples)
-    plt.show()
+    print(RX_CONSTELLATION_BUFFER)
+    plot_rx_constellation()
 
-    rx_baseband_samples = down_converter_rx(tx_continuous_samples)
-    rx_vectors = fft_rx(rx_baseband_samples)
-    data = symbol_map_rx(rx_vectors)
+    try:
+        print(f"Received {data.decode()}")
+    except:
+        pass
 
-    print(f"Received {data.decode()}")
+
